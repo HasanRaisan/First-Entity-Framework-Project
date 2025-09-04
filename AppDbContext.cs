5d3371b9883a1290489d48ae0c279e7d920a2fd9
Hasan Raisan <contact@example.com>
2025-09-04 12:52:06 +0300

    first commit

---
+++ AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity_Framework
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Connections.ConnectionString);
+        }
+
+        public DbSet<Shared.Models.Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Connections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework
+{
+    internal static class Connections
+    {
+        public const string ConnectionString = """
+                Server=HASSAN;
+                Database=EFCoreTestDB;
+                User Id=sa;
+                Password=sa123456;
+                TrustServerCertificate=True;
+                """;
+    }
+}
